6a7d36de66fb32c392c58a7a63dd289107a2920b
yagolucas2020 <yagolucas21.yl@gmail.com>
2022-08-16 11:52:03 -0300

    configurando tela de Login, Menu e o Dashboard

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return 'R$' + number_format(value);
           }
         },
         gridLines: {
